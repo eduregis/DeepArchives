e3494420edb232d27d9576a89056f23d286448ac
Eduardo Regis <eduregis1@gmail.com>
2020-11-20 09:57:18 -0300

    adding component and custom colors

--- DeepSheet/ViewController.swift
+++ DeepSheet/ViewController.swift
@@ -2,18 +2,36 @@
 //  ViewController.swift
 //  DeepSheet
 //
-//  Created by Eduardo Oliveira on 20/11/20.
+//  Created by Eduardo Oliveira on 17/11/20.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var attributesInformation: AttributesView = {
+        let attributes = AttributesView()
+        attributes.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(attributes)
+        return attributes
+    }()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .black
+            super.viewDidLoad()
+        additionalConfigurations()
     }
-
-
+    
+    private func additionalConfigurations() {
+        configureLayout()
+        //Change background color
+        view.backgroundColor = .backgroundBlack
+        //Navigation configuration
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            attributesInformation.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 32),
+            attributesInformation.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            attributesInformation.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16)
+        ])
+    }
 }
-
