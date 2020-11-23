776b0b57d3964aac7867c69ebe8d48aae9c46466
Eduardo Regis <eduregis1@gmail.com>
2020-11-23 10:20:05 -0300

    assets and fonts

---
+++ DeepSheet/Extensions/CustomFonts.swift
@@ -0,0 +1,32 @@
+//
+//  CustomFonts.swift
+//  DeepSheet
+//
+//  Created by Eduardo Oliveira on 23/11/20.
+//
+
+import UIKit
+
+extension UIFont {
+    class func josefinSansBold() -> UIFont {
+        guard let font = UIFont(name: "JosefinSans-Bold", size: 24) else {
+            fatalError("""
+               Failed to load the "JosefinSans-Bold" font.
+               Make sure the font file is included in the project and the font name is spelled correctly.
+               """
+           )
+        }
+        return font
+    }
+    
+    class func josefinSansRegular() -> UIFont {
+        guard let font = UIFont(name: "JosefinSans-Regular", size: 20) else {
+            fatalError("""
+               Failed to load the "JosefinSans-Regular" font.
+               Make sure the font file is included in the project and the font name is spelled correctly.
+               """
+           )
+        }
+        return font
+    }
+}
