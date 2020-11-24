e0dbd6f607937e05575b6df89fa405752361d4e3
Pedro Henrique Costa <henriquepcnews@gmail.com>
2020-11-24 15:32:10 -0300

    - Adds Group Points View using Points View Component - Adds Group Points View to View Controller

---
+++ DeepSheet/View/Components/GroupPointsView.swift
@@ -0,0 +1,85 @@
+//
+//  GroupPointsView.swift
+//  DeepSheet
+//
+//  Created by Pedro Henrique Costa on 24/11/20.
+//
+
+import UIKit
+
+class GroupPointsView: UIView {
+	
+	// MARK: - Components
+	lazy var luckView: IndvPointsView = {
+		let points = IndvPointsView(pointName: "SORTE", diceToggle: true, maxValue: 888)
+		points.translatesAutoresizingMaskIntoConstraints = false
+		self.addSubview(points)
+		return points
+	}()
+	
+	lazy var magicView: IndvPointsView = {
+		let points = IndvPointsView(pointName: "MAGIA", diceToggle: false, maxValue: 888)
+		points.translatesAutoresizingMaskIntoConstraints = false
+		self.addSubview(points)
+		return points
+	}()
+	
+	lazy var sanityView: IndvPointsView = {
+		let points = IndvPointsView(pointName: "SANIDADE", diceToggle: true, maxValue: 888)
+		points.translatesAutoresizingMaskIntoConstraints = false
+		self.addSubview(points)
+		return points
+	}()
+	
+	lazy var healthView: IndvPointsView = {
+		let points = IndvPointsView(pointName: "VIDA", diceToggle: false, maxValue: 888)
+		points.translatesAutoresizingMaskIntoConstraints = false
+		self.addSubview(points)
+		return points
+	}()
+	
+	lazy var pointsStack: UIStackView = {
+		let stack = UIStackView(arrangedSubviews: [luckView, magicView, sanityView, healthView])
+		stack.translatesAutoresizingMaskIntoConstraints = false
+		stack.axis = .vertical
+		stack.alignment = .fill
+		stack.distribution = .fillEqually
+		stack.spacing = 15
+		self.addSubview(stack)
+		return stack
+	}()
+	
+	lazy var groupLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		self.addSubview(label)
+		label.font = UIFont.josefinSansBold()
+		label.text = "Pontos"
+		label.textAlignment = .center
+		label.textColor = .ivory
+		label.backgroundColor = .clear
+		return label
+	}()
+	
+	init() {
+		super.init(frame: .zero)
+		configureLayout()
+	}
+	
+	// MARK: - Layout Constraints
+	private func configureLayout() {
+		NSLayoutConstraint.activate([
+			pointsStack.leftAnchor.constraint(equalTo: self.leftAnchor),
+			pointsStack.rightAnchor.constraint(equalTo: self.rightAnchor),
+			pointsStack.topAnchor.constraint(equalTo: self.topAnchor),
+			pointsStack.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+			
+			groupLabel.leftAnchor.constraint(equalTo: self.leftAnchor),
+			groupLabel.bottomAnchor.constraint(equalTo: pointsStack.topAnchor, constant: -5)
+		])
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented, because will not be used on IB")
+	}
+}
--- DeepSheet/View/ViewController.swift
+++ DeepSheet/View/ViewController.swift
@@ -15,8 +15,8 @@ class ViewController: UIViewController {
         return attributes
     }()
 	
-	lazy var pointsView: IndvPointsView = {
-		let points = IndvPointsView(pointName: "SORTE", diceToggle: true, maxValue: 888)
+	lazy var pointsView: GroupPointsView = {
+		let points = GroupPointsView()
 		points.translatesAutoresizingMaskIntoConstraints = false
 		self.view.addSubview(points)
 		return points
@@ -38,11 +38,9 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([
             attributesInformation.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 32),
             attributesInformation.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-            attributesInformation.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16)
-        ])
-		
-		NSLayoutConstraint.activate([
-			pointsView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 440),
+            attributesInformation.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+        
+			pointsView.topAnchor.constraint(equalTo: attributesInformation.bottomAnchor, constant: 55),
 			pointsView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16),
 			pointsView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16)
 		])
