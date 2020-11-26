d22938a7945800c5b88028b8209be9e757cdc12c
Gustavo Lemos <gsvieira08@gmail.com>
2020-11-26 09:48:08 -0300

    Added extension for dashed border

---
+++ DeepSheet/View/Components/AdditionCardView.swift
@@ -0,0 +1,23 @@
+//
+//  AdditionCardView.swift
+//  DeepSheet
+//
+//  Created by Gustavo Lemos on 26/11/20.
+//
+
+import UIKit
+
+class AdditionCardView: UIView {
+
+    // MARK: - Components
+    lazy var additionCardBorder {
+        
+    }
+    lazy var additionCardLabel: UILabel {
+        
+    }
+    lazy var additionCardPlusSign: UIImage {
+        
+    }
+
+}
