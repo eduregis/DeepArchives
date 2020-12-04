91c9a59c61d77241422c98c7e8d7884461945855
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2020-12-04 11:26:57 -0300

    nada feito

---
+++ DeepSheet/DataSource/CharacterState+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CharacterState+CoreDataClass.swift
+//  DeepSheet
+//
+//  Created by Rodrigo Silva Ribeiro on 04/12/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CharacterState)
+public class CharacterState: NSManagedObject {
+
+}
---
+++ DeepSheet/Model/State.swift
@@ -0,0 +1,8 @@
+//
+//  State.swift
+//  DeepSheet
+//
+//  Created by Rodrigo Silva Ribeiro on 04/12/20.
+//
+
+import Foundation
