a3f888086fb0fb49b378d57633a9acdd99dc2591
Gustavo Lemos <gsvieira08@gmail.com>
2020-12-09 09:10:57 -0300

    Correção das entidades e adição de relações

---
+++ DeepSheet/Model/Aspects+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Aspects+CoreDataClass.swift
+//  DeepSheet
+//
+//  Created by Gustavo Lemos on 09/12/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Aspects)
+public class Aspects: NSManagedObject {
+
+}
---
+++ DeepSheet/Model/AspectsStates+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  AspectsStates+CoreDataClass.swift
+//  DeepSheet
+//
+//  Created by Gustavo Lemos on 09/12/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(AspectsStates)
+public class AspectsStates: NSManagedObject {
+
+}
